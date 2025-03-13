1404dbc584b151988d452dd438cca3e2f6139020
Salmoon16 <alfarizijunior123@gmail.com>
2025-03-13 16:07:27 +0700

    salman : Mengisi file enum (case CREATE, UPDATE, DELETE, ERROR)

--- app/Enums/MessageType.php
+++ app/Enums/MessageType.php
@@ -2,6 +2,20 @@
 
 namespace App\Enums;
 
-enum MessageType : string {
-    
+enum MessageType: string
+{
+    case CREATED = 'Berhasil  Menambahkan';
+    case UPDATED = 'Berhasil  Memperbarui';
+    case DELETED = 'Berhasil  Mengahpus';
+    case ERROR = 'Terjadi Kesalahan, Silahkan Coba Lagi Nanti';
+
+    public function message(string $entity = '', ?string $error = null): string
+
+    {
+        if ($this == MessageType::ERROR && $error) {
+            return "{$this->value} {$error} {$error}";
+        }
+
+        return "{$this->value} {$entity}";
+    }
 }
