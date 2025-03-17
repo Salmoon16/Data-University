0f42b346f2e5de1a4af2f14a876ef063b52b1baf
Salmoon16 <alfarizijunior123@gmail.com>
2025-03-17 09:09:19 +0700

    salman : Menambahkan model dan migrasi Mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'fakultas_id',
+        'departement_id',
+        'classroom_id',
+        'fee_group_id',
+        'mahasiswa_number',
+        'semester',
+        'batch',
+    ];
 }
--- database/migrations/2025_03_17_015349_create_mahasiswas_table.php
+++ database/migrations/2025_03_17_015349_create_mahasiswas_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('mahasiswas', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('fakultas_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('departement_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('classroom_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('mahasiswa_number')->unique();
+            $table->unsignedInteger('semester')->default(1);
+            $table->year('batch');
+            $table->foreignId('fee_group_id')->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
         });
     }
