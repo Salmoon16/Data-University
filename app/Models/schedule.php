0f95dedcab0d154e59eef978605030f8967070c6
Salmoon16 <alfarizijunior123@gmail.com>
2025-03-19 08:08:19 +0700

    salman : Menambahkan Moel dan Migrasi Schedule

---
+++ app/Models/schedule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ScheduleDay;
+use Illuminate\Database\Eloquent\Model;
+
+class schedule extends Model
+{
+    protected $fillable = [
+        'fakultas_id',
+        'departement_id',
+        'course_id',
+        'classroom_id',
+        'academic_year_id',
+        'start_time',
+        'end_time',
+        'day_of_week',
+        'quota',
+        ];
+
+        protected function casts(): array
+        {
+            return [
+                'day_of_week' => ScheduleDay::class,
+            ];
+        }
+}
